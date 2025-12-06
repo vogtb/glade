2b11c9b2b625fd2be8ba13240a222919b6c52724
Ben Vogt <benjvogt@gmail.com>
2025-12-06 09:49:48 -0800

    cleaning up darwin code

--- packages/darwin/darwin.ts
+++ packages/darwin/darwin.ts
@@ -8,6 +8,7 @@ export {
 } from "./context.ts";
 export { glfw } from "@glade/glfw";
 export { lib, DarwinWebGL2RenderingContext } from "./opengl.ts";
+export * from "./webgpu.ts";
 
 // GLSL shader constants for OpenGL 3.2 Core Profile
 export const GLSL_VERSION = "#version 150 core";
