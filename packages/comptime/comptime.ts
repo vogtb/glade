47762d7ba7acfda75dcc6ba62540e9fcd76235de
Ben Vogt <benjvogt@gmail.com>
2026-01-04 12:27:33 -0800

    fixing some comptime ts bugs

--- packages/comptime/comptime.ts
+++ packages/comptime/comptime.ts
@@ -2,8 +2,6 @@
  * Bun macro to embed files at build time. This runs during bundling and
  * inlines the result.
  */
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck Don't type check this because it's just a Bun embed macro.
 async function embedAsBase64(path: string): Promise<string> {
   const file = Bun.file(path);
   const buffer = await file.arrayBuffer();
@@ -14,8 +12,6 @@ async function embedAsBase64(path: string): Promise<string> {
  * Bun macro to embed environment variables at build time.
  */
 async function embedEnvVar(name: string, defaultValue?: string): Promise<string> {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   const value = process.env[name] ?? defaultValue;
   if (!value) {
     throw new Error(`comptime process.env.${name} is not set, no default provided`);
@@ -44,14 +40,17 @@ async function embedBuildTime(): Promise<string> {
  * NOTE: We also prefix this with COMPTIME_ to be extract clear that this fn
  * is executed, well, at compile time, otherwise it can be confusing.
  */
-export const COMPTIME_embedAsBase64 = embedAsBase64 as (p: string) => string;
+export const COMPTIME_embedAsBase64 = embedAsBase64 as unknown as (p: string) => string;
 
 /**
  * Embeds an environment variable value at compile time, defaulting to a specific value.
  */
-export const COMPTIME_envVar = embedEnvVar as (name: string, defaultValue?: string) => string;
+export const COMPTIME_envVar = embedEnvVar as unknown as (
+  name: string,
+  defaultValue?: string
+) => string;
 
 /**
  * Embeds the current build time as an ISO string at compile time.
  */
-export const COMPTIME_buildTime = embedBuildTime as () => string;
+export const COMPTIME_buildTime = embedBuildTime as unknown as () => string;
